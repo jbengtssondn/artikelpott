096cfb43750f148e473cb2d285cb105081a013b6
unknown <johan.bengtsson@DNWGRCVM12.ad.bonniernews.se>
2015-08-20 14:03:31 +0200

    added some stuff

--- chrome-extention/modal.js
+++ chrome-extention/modal.js
@@ -35,7 +35,26 @@
  
  function initButtonEvents() {
     $("#videoButton").click(videoButtonClick);
+    // $("#referalButton").click(showAlternativeContainer("#referelContainer"));
+    // $("#klarnaButton").click(showAlternativeContainer("#klarnaContainer"));
+     $("#shareButton").click(shareButtonClick);
  }
+ 
+ 
+ function hideAlternatives() {
+    $("#buttonsContainer").hide();
+ };
+
+//SHARE START
+function shareButtonClick() {
+    hideAlternatives();
+    $("#shareContainer").show();
+    
+  }
+
+//END SHARE
+
+//START VIDEO FUNCTION
 
 $.getScript("http://csp.screen9.com/js/ultralight.js");
 var screen9_ajax_auth = "QHXsCbtEUhagtECcyKU9-9-ipMIr0ryNinYqdjg4J9sKtOmjOstMvA";
@@ -43,12 +62,13 @@ var screen9_ajax_auth = "QHXsCbtEUhagtECcyKU9-9-ipMIr0ryNinYqdjg4J9sKtOmjOstMvA"
 function videoEnded() {
     // TODO: increment artikelpott
     console.log("video ended");
-}
+};
+
 function videoEmbedded(data) { data.player.bind("finish", videoEnded); }
 function videoFailed(reason) { console.log(reason); }
 
 function videoButtonClick() {
-    $("#buttonsContainer").hide();
+    hideAlternatives();
     $("#videoContainer").show();
     var options = {
 	"mediaid": "2ZYXjBfcAr3ldYltl_OMoA",
@@ -58,3 +78,8 @@ function videoButtonClick() {
     };
     screen9.api.embed(options, videoEmbedded, videoFailed);
  };
+
+
+//END VIDEO FUNCTION
+
+
